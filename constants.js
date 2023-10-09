5c8ea3102e268a26bc07e4d8d0d1a3d2344a71ec
Vince Elizaga <vncelizaga@gmail.com>
2023-10-09 11:17:14 -0400

    update constants

--- constants.js
+++ constants.js
@@ -1,4 +1,4 @@
-const mqttEndpoint = 'mqtts://a2t5fqt4jh18lz-ats.iot.us-east-1.amazonaws.com:8883';
+const mqttEndpoint = `mqtts://${process.env.AWS_ROVY_MQTT_ENDPOINT}:8883`;
 const mqttMotorChannel = 'rovy/motor/c7de5c35-3882-48bc-bac6-f67fcf50da0f';
 
 module.exports = {
